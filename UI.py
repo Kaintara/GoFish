7413f58aa4c14e635070b0fd7a2b3d6fe95e40bd
Kaintara <barli2k@gmail.com>
2025-09-20 23:01:26 +0100

    Started on the UI for the game, learnt how to create and switch screens.

---
+++ UI.py
@@ -0,0 +1,27 @@
+from kivy.app import App
+from kivy.uix.screenmanager import ScreenManager, Screen
+
+
+class SM(ScreenManager):
+    pass
+
+class MainMenu(Screen):
+    pass
+
+class InGame(Screen):
+    pass
+
+class Settings(Screen):
+    pass
+
+class GoFish(App):
+    def build(self):
+        sm = SM()
+        sm.add_widget(MainMenu(name="Menu"))
+        sm.add_widget(InGame(name="InGame"))
+        sm.add_widget(Settings(name="Settings"))
+        return sm
+
+
+if __name__ == "__main__":
+    GoFish().run()
