d40d30900920fe44ba5fcece3a347efbaf6a8e75
Kaintara <barli2k@gmail.com>
2025-10-24 21:27:27 +0100

    Attempted to make a rotating settings button but failed miserably...

--- main.py
+++ main.py
@@ -1,15 +1,29 @@
 #Imports
-from kivymd.app import MDApp
-from kivymd.uix.screenmanager import MDScreenManager
-from kivymd.uix.screen import MDScreen
-from kivymd.uix.button import MDButton
+
+#Kivy
 from kivy.core.window import Window
 from kivy.core.text import LabelBase
 from kivy.metrics import sp
+from kivy.animation import Animation
+
+
+#KivyMD
+from kivymd.app import MDApp
+from kivymd.uix.screenmanager import MDScreenManager
+from kivymd.uix.screen import MDScreen
+from kivymd.uix.button import MDButton, MDButtonIcon
+from kivymd.uix.behaviors import RotateBehavior
+from kivymd.uix.boxlayout import MDBoxLayout
+
+
 
 #Custom Buttons/Cards
 class Playing_Card(MDButton):
     pass
+
+class Settings_Btn(RotateBehavior, MDBoxLayout):
+    pass
+
 
 #Screens
 class SM(MDScreenManager):
@@ -35,9 +49,8 @@ class GoFishApp(MDApp):
         #App Theming
         self.theme_cls.theme_style = "Light"
         self.theme_cls.primary_palette = "Pink"
-        self.theme_cls.primary_hue = "1000"
-        self.theme_cls.theme_style_switch_animation = True
-        self.theme_cls.theme_style_switch_animation_duration = 0.8
+        self.theme_cls.primary_hue = "100"
+        self.theme_cls.theme_style_switch_animation_duration = 0.4
 
         Window.set_icon(("icon.png"))
         #App Default Font & Font Styles
@@ -63,7 +76,7 @@ class GoFishApp(MDApp):
                 "font-size": sp(36),
             },
         }
-        #Assigning
+        #Assigning Screens to Screen Manager
         sm = SM()
         sm.add_widget(MainMenu(name="Menu"))
         sm.add_widget(InGame(name="InGame"))
@@ -71,6 +84,24 @@ class GoFishApp(MDApp):
         sm.add_widget(Settings(name="Settings"))
         sm.current = "Menu"
         return sm
+
+    def settings(self,widget):
+        current = self.root.current
+        self.root.transition.duration = 0
+
+        if current == "Menu": #Opening Settings
+            
+            anim = Animation(rotate_value_angle=90, d=0.3)
+            anim.bind(on_complete=lambda *x: self.switch_screen("Settings"))
+            anim.start(widget)
+
+        else: #Close Settings
+            anim = Animation(rotate_value_angle=0, d=0.3)
+            anim.bind(on_complete=lambda *x: self.switch_screen("Menu"))
+            anim.start(widget)
+
+    def switch_screen(self, screen_name):
+        self.root.current = screen_name
 
 #Running App
 if __name__ == "__main__":
